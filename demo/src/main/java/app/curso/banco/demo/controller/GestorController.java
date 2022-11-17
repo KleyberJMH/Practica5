07a1257d6f1904be7eb214ae5167c2dcaf323198
kleyberJMH <kleyberjmh@gmail.com>
2022-11-17 20:03:41 +0100

    CORS

--- demo/src/main/java/app/curso/banco/demo/controller/GestorController.java
+++ demo/src/main/java/app/curso/banco/demo/controller/GestorController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import app.curso.banco.demo.model.Gestor;
 import app.curso.banco.demo.service.GestorService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path="/gestor")
 public class GestorController {
 	@Autowired
--- demo/src/main/java/app/curso/banco/demo/controller/TransferenciaController.java
+++ demo/src/main/java/app/curso/banco/demo/controller/TransferenciaController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import app.curso.banco.demo.model.Transferencia;
 import app.curso.banco.demo.service.TransferenciaService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(path="/transferencia")
 public class TransferenciaController {
 	@Autowired
